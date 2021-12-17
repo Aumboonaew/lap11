99f6ca6de1dfbb88403b50a222207176a52f33be
Aumboon <unaweblanca@gmail.com>
2021-12-17 23:52:55 +0700

    commit

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+
+using namespace std;
+
+int main(){
+    double count = 0;
+    float sum = 0;
+    float S = 0;
+    string t;
+    ifstream sco("score.txt");
+    while(getline(sco,t)){
+        sum += atof(t.c_str());
+        S = sqrt(0.2161916929/(count - 1));
+        count++;
+    }
+
+    cout << "Number of data = " << count << '\n';
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << '\n';
+    cout << "Standard deviation = " << S << '\n';
+}
